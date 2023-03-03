02de20fc66a6487e7d230ae9bdf311bb81034156
Abdelrahman Mahmoud <74074348+Abd0lRahman@users.noreply.github.com>
2023-03-03 20:01:30 +0200

    Add files via upload

---
+++ STD_TYPES.h
@@ -0,0 +1,21 @@
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+/* Address NULL */
+#define NULL ( (void *) 0x00)
+
+/* Signed Data Types */
+typedef unsigned char           uint8_t;
+typedef unsigned short int      uint16_t;
+typedef unsigned long int       uint32_t;
+
+/* Unsigned Data Types */
+typedef signed char             int8_t;
+typedef signed short int        int16_t;
+typedef signed long int         int32_t;
+
+/* floating Data Types */
+typedef float f32;
+typedef double f64;
+
+#endif
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "STD_TYPES.h"
+#include "Application/app.h"
+
+int main()
+{
+    while(1)
+    {
+       appStart();
+    }
+
+}
