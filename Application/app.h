92af776c19525447c5c52204f7c7a487a9a1d3b0
Abdelrahman Mahmoud <74074348+Abd0lRahman@users.noreply.github.com>
2023-03-03 20:02:42 +0200

    Add files via upload

---
+++ Application/app.h
@@ -0,0 +1,9 @@
+#ifndef APP_H_
+#define App_H_
+#include "../STD_TYPES.h"
+#include "../card/card.h"
+#include "../terminal/terminal.h"
+#include "../server/server.h"
+void appStart(void);
+
+#endif
